8fc74040afb0fa140e54a7265db147c20c3ce3a1
rushabhkhatavkar79 <rushabh.khatavkar@gmail.com>
2019-12-18 17:36:29 +0530

    space

--- hrms/src/components/Directory.js
+++ hrms/src/components/Directory.js
@@ -17,11 +17,11 @@ function Directory(props) {
             <div style={{ borderBottom: "1px solid black" }}>
 
                 <strong>
-                    {di.employeeid},
+                    {di.employeeid},{" "}
                     {di && di.personalinfo ? di.personalinfo.fullname : null}
                 </strong><br />
                 <strong>Email: </strong>{di && di.contactinfo ? di.contactinfo.workemail : null},
-                <strong>Mobile: </strong>{di && di.contactinfo ? di.contactinfo.mobile : null}<br />
+                <strong> Mobile: </strong>{di && di.contactinfo ? di.contactinfo.mobile : null}<br />
                 <strong>Project: </strong>{di && di.projectinfo ? di.projectinfo.projectdescription : null}
                 <br />
             </div>
